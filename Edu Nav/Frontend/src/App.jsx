41dc747548c35e215402f23ab1d7f908e17c5045
Jatin <jatinpathania1@gmail.com>
2025-09-16 20:18:23 +0530

    app routes

--- Edu Nav/Frontend/src/App.jsx
+++ Edu Nav/Frontend/src/App.jsx
@@ -3,23 +3,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-import SignIn from './components/Auth/SignIn';
-import SignUp from './components/Auth/SignUp'; 
-import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/signup" element={<SignUp />} />
-
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-
         </Routes>
 
         <Footer />
