74964a66f245c1f0162c489b6a5d5ceae5b3a022
Jatin <jatinpathania1@gmail.com>
2025-09-17 22:45:15 +0530

    Main layout containing both header and footer

---
+++ Edu Nav/Frontend/src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header/>
+      <main className="flex-grow pt-20">
+        {children}
+      </main>
+      <Footer/>
+    </div>
+  );
+};
+
+export default MainLayout;
